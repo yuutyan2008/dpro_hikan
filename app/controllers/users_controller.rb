223820dc9dd5ccf6ab4b55d566e61c3fc8dd1dd4
takanosatomi <ricebread778899@gmail.com>
2024-10-04 10:45:31 +0900

    first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,45 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  
+
   def create
+
     @user = User.new(user_params)
+
+    # ユーザのログインが完了したら悲観的ロックを適用
     if @user.save
+      # ログインする
       log_in(@user)
-      redirect_to new_order_path
-    else
-      render :new
+
+      ActiveRecord::Base.transaction do
+
+
+        @orders = Order.find_by(id: params[:order_id])
+
+        # @ordersがnilかどうか確認
+        if @orders.nil?
+          flash[:error] = "Order not found"
+          render :new
+
+          raise ActiveRecord::Rollback  # トランザクションをロールバック
+            # 商品を購入不可に更新する
+            @orders.update!(available: false)
+          
+
+          # 成功時のリダイレクト
+          redirect_to new_order_path
+        else
+          # ユーザーの登録に失敗した場合の処理
+          render :new
+        end
+      end
     end
   end
+
+
+
+
 
   private
 
